5e68d9a11e8af55b2fec075397931c5cf6d9eb83
denis <denislegrace@yandex.ru>
2022-07-05 09:03:58 +0500

    docker update

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ docker build --network=host -t raspservicei ./
 docker stop raspservice
 docker rm raspservice
 #docker run --name raspservice -d --restart unless-stopped raspservicei
-docker run --name raspservice -d --restart unless-stopped -v /sys:/sys -d raspservicei
+docker run --name raspservice -d --restart unless-stopped -v /dev/gpiomem:/dev/gpiomem -d raspservicei
 
