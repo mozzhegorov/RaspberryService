b8d3bd806d253a343e6217b9035d15789f0658d6
denis <denislegrace@yandex.ru>
2022-07-05 09:10:59 +0500

    docker update

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ docker build --network=host -t raspservicei ./
 docker stop raspservice
 docker rm raspservice
 #docker run --name raspservice -d --restart unless-stopped raspservicei
-docker run --name raspservice -d --privileged --restart unless-stopped -v /dev/gpiomem:/dev/gpiomem -d raspservicei
+docker run --name raspservice -d --privileged --restart unless-stopped -v /dev/gpiomem:/dev/gpiomem -v /usr/bin/vcgencmd:/usr/bin/vcgencmd -d raspservicei
 
