58848cca664627ea9684c18741eef391f327b2ea
denis <denislegrace@yandex.ru>
2022-07-08 16:10:34 +0500

    docker update

--- update.sh
+++ update.sh
@@ -3,5 +3,6 @@ docker build --network=host -t raspservicei ./
 docker stop raspservice
 docker rm raspservice
 #docker run --name raspservice -d --restart unless-stopped raspservicei
-sudo docker run --name raspservice -d --privileged --restart unless-stopped -v /dev/gpiomem:/dev/gpiomem -v /usr/bin/vcgencmd:/usr/bin/vcgencmd -d raspservicei
+sudo docker run --name raspservice -d --privileged --restart unless-stopped -v /dev/gpiomem:/dev/gpiomem \
+ -v /usr/lib/aarch64-linux-gnu/libvchiq_arm.so.0:/usr/lib/aarch64-linux-gnu/libvchiq_arm.so.0 -v /usr/lib/aarch64-linux-gnu/libvchiq_arm.so.0:/usr/lib/aarch64-linux-gnu/libvchiq_arm.so.0 -d raspservicei
 
