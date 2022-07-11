096cdbe8974f0e1ec47e846955c053204b724721
denis <denislegrace@yandex.ru>
2022-07-11 21:26:12 +0500

    commit for testing

--- db.py
+++ db.py
@@ -24,8 +24,9 @@ SETTINGS_EXISTS = """
 """
 CREATE_SETTINGS_TABLE = """
     CREATE TABLE IF NOT EXISTS settings (
-        id VARCHAR(50) PRIMARY KEY,
-        value VARCHAR(50)
+        id VARCHAR(50),
+        value VARCHAR(50),
+        PRIMARY KEY (id)
     );
 """
 CREATE_HISTORY_TABLE = """
