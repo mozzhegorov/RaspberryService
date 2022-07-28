19f704ccd485121e2278db97f9d75f14b7c01bde
denis <denislegrace@yandex.ru>
2022-07-29 00:43:13 +0500

    adding cron

--- raspberry/start.sh
+++ raspberry/start.sh
@@ -3,6 +3,8 @@
 #cd "$(dirname "$0")";
 #CWD="$(pwd)"
 #echo $CWD
+printenv | grep -v "no_proxy" >> /etc/environment
+cron -f
 PATH=/usr/local/bin/
 /usr/local/bin/python /home/main.py
 touch /home/test.txt
