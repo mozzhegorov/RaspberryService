dbbbe12566db1cf0af85f5c871254c6c976bb78a
denis <denislegrace@yandex.ru>
2022-07-28 22:43:27 +0500

    adding cron

--- raspberry/main.py
+++ raspberry/main.py
@@ -23,6 +23,8 @@ rasp_temp = Temperature()
 if __name__ == "__main__":
     create_tables()
     print("Script run")
+    send_telegram("\ура ")
+
     try:
         temp_on = rasp_temp.high_temp
         control_pin = int(CONTROL_PIN)
