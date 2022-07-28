4c08675ecc984127a4dcbe88ba7e68f764ed41b5
denis <denislegrace@yandex.ru>
2022-07-29 00:15:34 +0500

    adding cron

--- raspberry/start.sh
+++ raspberry/start.sh
@@ -3,5 +3,6 @@
 #cd "$(dirname "$0")";
 #CWD="$(pwd)"
 #echo $CWD
-#python main.py
+PATH=/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+python main.py
 touch /home/test.txt
