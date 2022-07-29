f4dd79d4481ce0f862f730e43930f1ad809b1b5a
denis <denislegrace@yandex.ru>
2022-07-29 11:57:11 +0500

    telegram msg high temp

--- raspberry/teleecho.py
+++ raspberry/teleecho.py
@@ -1,4 +1,11 @@
+from raspberry.services import get_temp
 from requesting import send_telegram
 
 if __name__ == "__main__":
     send_telegram("RaspberryPi на связи")
+    rasp_temp = get_temp()
+    if rasp_temp > 75:
+        send_telegram("Повышенная температура Raspberry")
+
+
+
