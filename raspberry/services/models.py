1c122a904b4e191a1c0df492a857440c04be1c4c
denis <denislegrace@yandex.ru>
2022-07-29 15:46:47 +0500

    update project

--- raspberry/services/models.py
+++ raspberry/services/models.py
@@ -12,11 +12,12 @@ from services.settings import (
 )
 
 DB_CONFIG = {
-    'dbname': DB_NAME,
-    'user': DB_USER,
-    'password': DB_PASSWORD,
+    'drivername': 'postgresql+psycopg2',
     'host': DB_HOST,
     'port': DB_PORT,
+    'username': DB_USER,
+    'password': DB_PASSWORD,
+    'database': DB_NAME,
 }
 # DB_CONFIG = env.get_value('DB_CONFIG')
 
