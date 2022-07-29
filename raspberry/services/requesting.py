a85177b2dea1692dcb019afc966964f3b197c459
denis <denislegrace@yandex.ru>
2022-07-29 15:13:07 +0500

    update project

--- raspberry/services/requesting.py
+++ raspberry/services/requesting.py
@@ -4,17 +4,16 @@ from services.settings import BOT_TOKEN, CHANEL_ID
 
 def send_telegram(text):
     token = BOT_TOKEN
-    print(BOT_TOKEN, CHANEL_ID)
     url = "https://api.telegram.org/bot"
     channel_id = CHANEL_ID
     url += token
     method = url + "/sendMessage"
+    print(token, channel_id, text, method)
 
     r = requests.post(method, data={
         "chat_id": channel_id,
         "text": text
     })
-    print(r)
 
     if r.status_code != 200:
         print(r)
