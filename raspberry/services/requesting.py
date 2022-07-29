36efe7b349235fe81f77267d43efe85fc19e1a31
denis <denislegrace@yandex.ru>
2022-07-29 15:04:08 +0500

    update project

--- raspberry/services/requesting.py
+++ raspberry/services/requesting.py
@@ -4,6 +4,7 @@ from services.settings import BOT_TOKEN, CHANEL_ID
 
 def send_telegram(text):
     token = BOT_TOKEN
+    print(BOT_TOKEN)
     url = "https://api.telegram.org/bot"
     channel_id = CHANEL_ID
     url += token
