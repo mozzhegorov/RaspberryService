975d11b11ddc1223e7b74ee68f69ec51eb1d1b9f
denis <denislegrace@yandex.ru>
2022-07-29 15:00:37 +0500

    update project

--- raspberry/services/settings.py
+++ raspberry/services/settings.py
@@ -3,10 +3,12 @@ import configparser
 config = configparser.ConfigParser()
 config.read('.env')
 
-BOT_TOKEN = config['BOT_TOKEN']
-CHANEL_ID = config['CHANEL_ID']
-DB_NAME = config['DB_NAME']
-DB_USER = config['DB_USER'],
-DB_PASSWORD = config['DB_PASSWORD']
-DB_HOST = config['DB_HOST']
-DB_PORT = config['DB_PORT']
+general_env = config["General"]
+
+BOT_TOKEN = general_env['BOT_TOKEN']
+CHANEL_ID = general_env['CHANEL_ID']
+DB_NAME = general_env['DB_NAME']
+DB_USER = general_env['DB_USER']
+DB_PASSWORD = general_env['DB_PASSWORD']
+DB_HOST = general_env['DB_HOST']
+DB_PORT = general_env['DB_PORT']
