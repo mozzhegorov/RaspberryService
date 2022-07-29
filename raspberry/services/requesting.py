904b1f1b6cee0c0d736d779161da9cd3899dac94
denis <denislegrace@yandex.ru>
2022-07-29 14:34:49 +0500

    update project

--- raspberry/services/requesting.py
+++ raspberry/services/requesting.py
@@ -1,22 +1,22 @@
 import requests
-from services.settings import BOT_TOKEN, CHANEL_ID
-
-
-def send_telegram(text):
-    token = BOT_TOKEN
-    url = "https://api.telegram.org/bot"
-    channel_id = CHANEL_ID
-    url += token
-    method = url + "/sendMessage"
-
-    r = requests.post(method, data={
-        "chat_id": channel_id,
-        "text": text
-    })
-
-    if r.status_code != 200:
-        print(r)
-        raise Exception("post_text error")
+# from services.settings import BOT_TOKEN, CHANEL_ID
+#
+#
+# def send_telegram(text):
+#     token = BOT_TOKEN
+#     url = "https://api.telegram.org/bot"
+#     channel_id = CHANEL_ID
+#     url += token
+#     method = url + "/sendMessage"
+#
+#     r = requests.post(method, data={
+#         "chat_id": channel_id,
+#         "text": text
+#     })
+#
+#     if r.status_code != 200:
+#         print(r)
+#         raise Exception("post_text error")
 
 
 def prii():
