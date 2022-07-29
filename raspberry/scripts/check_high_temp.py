bd15ca8fd1b9314ac49dc50ffed2b6fd664937d6
denis <denislegrace@yandex.ru>
2022-07-29 22:30:55 +0500

    update project

--- raspberry/scripts/check_high_temp.py
+++ raspberry/scripts/check_high_temp.py
@@ -3,6 +3,5 @@ from services.requesting import send_telegram
 
 if __name__ == "__main__":
     rasp_temp = get_rasp_temp()
-    send_telegram(f"Проверка температуры {rasp_temp}")
     if rasp_temp > 75:
         send_telegram("Повышенная температура Raspberry")
